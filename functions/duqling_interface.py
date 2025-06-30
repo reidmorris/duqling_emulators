281dafc3122e128c2fff0bedca2b93c73cc4f4c4
reidmorris <100496249+reidmorris@users.noreply.github.com>
2025-06-30 13:41:09 -0700

    feat: support functions passed as kwargs

--- functions/duqling_interface.py
+++ functions/duqling_interface.py
@@ -3,20 +3,23 @@ May have to execute the following code in terminal first:
 > R -e "devtools::install_github('knrumsey/duqling')"
 """
 
-from typing import Tuple, Dict, Optional
-import warnings
+from typing import Tuple, Optional
 from pathlib import Path
 import numpy as np
 import pandas as pd
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
 from rpy2.robjects import pandas2ri, numpy2ri
+from rpy2.rinterface import rternalize
 
 # activate pandas and numpy conversion
 pandas2ri.activate()
 numpy2ri.activate()
 
 class DuqlingInterface:
+    """
+    Represents an interface that allows the R Duqling package to be callable in Python.
+    """
     def __init__(self, r_script_path:str="quack.R"):
         """
         Initialize the duqling interface.
@@ -34,7 +37,7 @@ class DuqlingInterface:
             robjects.r["source"](str(script_path))
             self.gen_func = robjects.globalenv["quack"]
         except Exception as e:
-            raise RuntimeError(f"Failed to initialize duqling interface: {e}")
+            raise RuntimeError(f"Failed to initialize duqling interface: {e}") from e
 
     def list_functions(self, **kwargs) -> pd.DataFrame:
         """
@@ -85,16 +88,18 @@ class DuqlingInterface:
             # not necessary, but makes it look nicer
             return {str(k): result[k] for k in result}
         except Exception as e:
-            raise ValueError(f"Function '{function_name}' not found: {e}")
-    
-    def generate_data(self, func_name:str, n_samples:int, seed:Optional[int]=42, **kwargs) -> Tuple[np.array, np.array]:
+            raise ValueError(f"Function '{function_name}' not found: {e}") from e
+
+    def generate_data(self, func_name:str, n_samples:int=None, X:Optional[np.array]=None,
+                      seed:Optional[int]=42, **kwargs) -> Tuple[np.array, np.array]:
         """
         Generate data using a duqling function.
         
         Args:
             func_name: Name of the duqling function
             n_samples: Number of samples to generate
-            seed: Random seed for reproducibility
+            X:         Input values to duqling function
+            seed:      Random seed for reproducibility
         Kwargs:
             Additional arguments to pass to the duqling function
         
@@ -102,10 +107,15 @@ class DuqlingInterface:
             (X, y)
         """
         try:
+            X = robjects.NULL if X is None else X
+            n_samples = robjects.NULL if n_samples is None else n_samples
             if kwargs:
-                result = self.gen_func(func_name, n_samples=n_samples, seed=seed, **kwargs)
+                for key, value in kwargs.items():
+                    if callable(value):
+                        kwargs[key] = rternalize(value)
+                result = self.gen_func(func_name, n_samples=n_samples, X=X, seed=seed, **kwargs)
             else:
-                result = self.gen_func(func_name, n_samples=n_samples, seed=seed)
+                result = self.gen_func(func_name, n_samples=n_samples, X=X, seed=seed)
 
             X = np.array(result.rx2("X"))
             y = np.array(result.rx2("y"))
@@ -113,4 +123,4 @@ class DuqlingInterface:
             return X, y
 
         except Exception as e:
-            raise RuntimeError(f"Failed to generate data for '{func_name}': {e}")
+            raise RuntimeError(f"Failed to generate data for '{func_name}': {e}") from e
