4d6fbec51489d8d6f588f0ce7cd071f386493aef
reidmorris <100496249+reidmorris@users.noreply.github.com>
2025-06-30 13:09:51 -0700

    feat: replace uniform sampling with LHS

--- functions/quack.R
+++ functions/quack.R
@@ -1,4 +1,12 @@
-quack <- function(function_name, n_samples, seed = NULL, ...) {
+lhs_samples <- function(n, d) {
+  samples <- matrix(0, n, d)
+  for (i in 1:d) {
+    samples[, i] <- sample((0:(n - 1) + runif(n)) / n)
+  }
+  samples
+}
+
+quack <- function(function_name, n_samples, X = NULL, seed = NULL, ...) {
   if (!is.null(seed)) {
     set.seed(seed)
   }
@@ -9,9 +17,14 @@ quack <- function(function_name, n_samples, seed = NULL, ...) {
     stop(paste("Function", function_name, "not found in duqling package"))
   })
 
-  input_dim <- func_info$input_dim
+  if (is.null(X)) {
+    if (is.null(n_samples)) {
+      stop(paste("n_samples and X cannot both be NULL"))
+    }
+    input_dim <- func_info$input_dim
+    X <- lhs_samples(n_samples, input_dim)
+  }
 
-  X <- matrix(runif(n_samples * input_dim), nrow = n_samples, ncol = input_dim)
   y <- duqling::duq(X, function_name, scale01 = TRUE, ...)
 
   list(X = X, y = y, func_info = func_info)
